20a2d68f5b88c06c022a9e8ad6f62498bb9a3ead
Juan Ayala <misterio562@gmail.com>
2023-03-09 21:44:21 -0500

    Importando el puerto del archivo config.js del server

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import morgan from "morgan";
 
+import { PORT } from "./config.js";
+
 /* Importando la función openConnection desde el archivo conexiondb.js */
 import { openConnection } from "../database/conexiondb.js";
 
@@ -109,6 +111,6 @@ app.get("/validate", function (req, res) {
   // }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port ",PORT);
 });
