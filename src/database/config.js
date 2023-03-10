4936bbf3a31f8835ee8300d67b646476411d59b7
Juan Ayala <misterio562@gmail.com>
2023-03-09 22:38:06 -0500

    Creando el archivo de configuracion de base de datos config.js , y dentro sus valores

---
+++ src/database/config.js
@@ -0,0 +1,5 @@
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+export const DB_USER = process.env.DB_USER || "userlingoadmin";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
+export const DB_NAME = process.env.DB_NAME || "lingostudentsdb";
+export const DB_PORT = process.env.DB_PORT || "3306";
