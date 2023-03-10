cc6f7ec8568ea125f9a60f7d1a4af104569adc00
Juan Ayala <misterio562@gmail.com>
2023-03-09 22:40:58 -0500

    Importando las configuraciones del archivo config.js a la conexion de la base de datos

--- src/database/conexiondb.js
+++ src/database/conexiondb.js
@@ -1,60 +1,13 @@
-// const mysql = require ('mysql2');
-
-// /**
-//  * Crea una conexión a la base de datos y la devuelve.
-//  * @returns El objeto de conexión.
-//  */
-// function openConnection() {
-//   const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: '123456',
-//     database: 'tiendaxdb'
-//   });
-
-//   connection.connect(function(err) {
-//       if (err) {
-//           console.error('Error al conectarse a la base de datos: ' + err.stack);
-//           return;
-//       }
-//       console.log('Conectado a la base de datos como ID ' + connection.threadId);
-//   });
-
-//   return connection;
-// }
-
-// /**
-//  * Cierra la conexión a la base de datos.
-//  * @param connection - El objeto de conexión.
-//  * @returns El objeto de conexión.
-//  */
-// function closeConnection(connection) {
-//   connection.end(function(err) {
-//       if (err) {
-//           console.error('Error al cerrar la conexión: ' + err.stack);
-//           return;
-//       }
-//       console.log('Conexión cerrada');
-//   });
-// }
-
-// /* Exportando las funciones `openConnection` y `closeConnection` para que puedan ser utilizadas en
-// otros archivos. */
-// module.exports = {
-//   openConnection,
-//   closeConnection
-// }
-
 import mysql from "mysql2";
+import {DB_HOST,DB_PORT,DB_USER,DB_PASSWORD,DB_NAME} from './config.js'
 
 export const openConnection = () => {
   const connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "userlingoadmin",
-    password: "123456",
-    database: "lingostudentsdb",
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
   });
 
   connection.connect(function (err) {
