80bb04cde262aac6dfda10a09e73e4d2ea96139f
Juan Ayala <misterio562@gmail.com>
2023-03-12 21:39:43 -0500

    Quitando los endpoint del index.js para usarlo en otro archivo. Importando los endpoint de otro archivo, y usándolos en index.js

--- src/server/index.js
+++ src/server/index.js
@@ -5,12 +5,9 @@ import morgan from "morgan";
 
 import { PORT } from "./config.js";
 
-/* Importando la función openConnection desde el archivo conexiondb.js */
-import { openConnection } from "../database/conexiondb.js";
+import endpointValidateUser from "./routes/endpointValidateUser.js";
 
 const app = express();
-/* Creación de una conexión a la base de datos. */
-const connection = openConnection();
 
 // Un middleware que analiza el cuerpo de la solicitud y lo pone a disposición en la propiedad req.body.
 app.use(express.json());
@@ -20,6 +17,9 @@ app.use(bodyParser.json());
 app.use(cors());
 // Un middleware que registra las solicitudes a la consola.
 app.use(morgan("dev"));
+
+//Se usan los endPoint
+app.use("/validate", endpointValidateUser);
 
 /* Una solicitud de obtención al servidor. */
 app.get("/registers", (req, res) => {
@@ -71,56 +71,6 @@ app.post("/register", (req, res) => {
   //     res.send({ message: "Datos guardados correctamente" });
   //   }
   // );
-});
-
-app.get("/validate", function (req, res) {
-  const email = req.query.email;
-  const sql = "SELECT * FROM student WHERE email = ?";
-  connection.query(sql, [email], function (err, results) {
-    if (err) {
-      // Manejar el error
-      console.log(err);
-      return res.status(500).send("Error al buscar usuario");
-    }
-
-    // Si la consulta no devuelve resultados, el email no existe en la base de datos
-    if (results.length === 0) {
-      return res.status(400).send("El usuario no existe");
-    }
-
-    // Si la consulta devuelve resultados, el email existe en la base de datos
-    const usuario = results[0];
-    return res.send(usuario);
-  });
-
-  // if (req.query.email==null) {
-  //   res.send("Está nulo")
-  // }else{
-  //   res.send("No está nulo")
-  // }
-
-  // const sql = "SELECT * FROM student WHERE email = ?";
-  // const rows = await connection.execute(sql, [req.query.email]);
-  // if (rows) {
-  //   //res.status(200).json({ message: 'Correo electrónico encontrado' });
-  //   res.send("Encontrado");
-  // } else {
-  //   res.send("No encontrado");
-  //   //res.status(404).json({ message: 'Correo electrónico no encontrado en la base de datos' });
-  // }
-});
-
-app.post("/validate", (req, res) => {
-  const email = req.body.email;
-  const displayName = req.body.displayName;
-
-  const sql = "INSERT INTO student (email, displayName) VALUES (?, ?)";
-  connection.query(sql, [email, displayName], (err) => {
-    if (err) {
-      return res.status(404).send("Error to save user");
-    }
-    return res.status(200).send("User saved to database");
-  });
 });
 
 app.post("/module1theme1", (req, res) => {
