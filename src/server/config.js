be46b003ad4d629f7b7abc09addf69f3b778ed19
Juan Ayala <misterio562@gmail.com>
2023-04-08 22:59:30 -0500

    cambiando localhost por la url de railway

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,3 @@
 /* Exportación del puerto y la URL del servidor que se usarán en el archivo index.js. */
 export const PORT = 3000;
-export const SERVER_URL = `http://localhost:${PORT}`;
+export const SERVER_URL = `lingostudents.up.railway.app${PORT}`;
