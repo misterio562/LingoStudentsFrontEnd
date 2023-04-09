84de4421614b5262bf955abef44ea76643fe9e1c
Juan Ayala <misterio562@gmail.com>
2023-04-08 22:16:17 -0500

    ajustando el start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Inicia la aplicación web
+yarn run dev
+
 # Inicia el servidor
 yarn run server &
 sleep 3 # Espera 3 segundos para asegurarte de que el servidor está corriendo
 
-# Inicia la aplicación web
-yarn run dev
+
