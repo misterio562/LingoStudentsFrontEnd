2a64270bdaa9aeda5218658b909b81192a575e91
Juan Ayala <misterio562@gmail.com>
2023-04-08 23:38:42 -0500

    cambiando configuracion de la base de datos

--- src/database/config.js
+++ src/database/config.js
@@ -1,11 +1,11 @@
-// export const DB_HOST = process.env.DB_HOST || "containers-us-west-174.railway.app";
-// export const DB_USER = process.env.DB_USER || "root";
-// export const DB_PASSWORD = process.env.DB_PASSWORD || "RMfiz5IYwOS97yRPQp2z";
-// export const DB_NAME = process.env.DB_NAME || "railway";
-// export const DB_PORT = process.env.DB_PORT || "6369";
+export const DB_HOST = process.env.DB_HOST || "containers-us-west-174.railway.app";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "RMfiz5IYwOS97yRPQp2z";
+export const DB_NAME = process.env.DB_NAME || "railway";
+export const DB_PORT = process.env.DB_PORT || "6369";
 
-export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
-export const DB_USER = process.env.DB_USER || "userlingoadmin";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
-export const DB_NAME = process.env.DB_NAME || "lingostudentsdb";
-export const DB_PORT = process.env.DB_PORT || "3306";
+// export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+// export const DB_USER = process.env.DB_USER || "userlingoadmin";
+// export const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
+// export const DB_NAME = process.env.DB_NAME || "lingostudentsdb";
+// export const DB_PORT = process.env.DB_PORT || "3306";
