2780624c49327672cb68a8f586a1a2cf4eee33c6
Juan Ayala <misterio562@gmail.com>
2023-04-08 22:30:08 -0500

    Revirtiendo cambios de start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Inicia la aplicación web
-yarn run dev
-
 # Inicia el servidor
 yarn run server &
 sleep 3 # Espera 3 segundos para asegurarte de que el servidor está corriendo
 
-
+# Inicia la aplicación web
+yarn run dev
