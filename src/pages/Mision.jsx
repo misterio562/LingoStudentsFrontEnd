fa93b135738d10e2ffb50f5fb4e532efe248ae2f
Daniout <denovoa@unicesar.edu.co>
2023-06-04 11:08:00 -0500

    agregando rutas vision y mision

--- src/pages/Mision.jsx
+++ src/pages/Mision.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import misionImage from '../assets/Images/Mision.gif';
+import './Modules/css/allFrom.css'
 
 const Mision = () => {
   return (
-    <div>Mision</div>
+    <div className="mision-container">
+    <div className="mision-content">
+      <img src={misionImage} alt="Misión" className="mision-image" />
+    </div>
+  </div>
   )
 }
 
---
+++ src/pages/Vision.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Vision = () => {
+    return (
+      <div className="mision-container">
+      <div className="mision-content">
+        <img src={misionImage} alt="Vision" className="mision-image" />
+      </div>
+    </div>
+    )
+  }
+  
+  export default Vision
