e537a150a26752b5770da56a89138beedb09b4d1
Juan Ayala <misterio562@gmail.com>
2023-09-29 10:31:05 -0500

    fix: :bug: add server url koyeb
    Se añadió la url del api rest de koyeb, en vez de usar localhost

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,2 +1,8 @@
-export const PORT = 4000;
-export const apiURL = `http://localhost:${PORT}`;
+// export const PORT = 4000;
+// export const apiURL = `http://localhost:${PORT}`;
+
+
+process.env.SERVER_PORT
+process.env.SERVER_URL
+
+export const apiURL = `${process.env.SERVER_URL}`;
