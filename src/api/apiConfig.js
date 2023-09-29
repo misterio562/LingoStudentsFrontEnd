c96e80ef814ca75bc219c50767a6f1c6f3eb5dfb
Juan Ayala <misterio562@gmail.com>
2023-09-29 10:38:44 -0500

    fix: :bug: comentando SERVER_PORT
    Se tuvo que comentar porque daba error

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -2,7 +2,8 @@
 // export const apiURL = `http://localhost:${PORT}`;
 
 
-process.env.SERVER_PORT
+// process.env.SERVER_PORT
+
 process.env.SERVER_URL
 
 export const apiURL = `${process.env.SERVER_URL}`;
